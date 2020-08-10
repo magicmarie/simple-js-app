20d54e226b91d07a4b745180bf4fc845fca55513
magicmarie <natukunda162@gmail.com>
2020-08-11 01:07:55 +0300

    add new array of objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,28 @@
-// 1
-alert('Hello world');
+pokemonList = [
+  {
+    name: 'Bulbasur',
+    height: 2.04,
+    types: ['grass','poison'],
+    abilities: 'overgrow'
+  },
+  {
+    name: 'Weedle',
+    height: 1.00,
+    types: ['bug','poison'],
+    abilities: 'Shield Dust'
+  },
+  {
+    name: 'Jigglypuff',
+    height: 1.08,
+    types: ['normal', 'fairy'],
+    abilities: ['cute charm', 'competitive']
+  },
+  {
+    name: 'Nidoqueen',
+    height: 4.03,
+    types: ['poison', 'ground'],
+    abilities: ['poison point', 'rivalry']
+  }
+];
 
-// 2
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+document.write(pokemonList);
