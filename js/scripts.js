6f70e6d4d52c90249af8d497e8af9e9745234718
magicmarie <natukunda162@gmail.com>
2020-08-12 10:38:37 +0300

    add forEach and IIFE functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,45 @@
-pokemonList = [
+var pokemonRepository = (function() {
+  var pokemonList = [
   {
-    name: 'Bulbasur',
-    height: 2.04,
-    types: ['grass','poison'],
-    abilities: 'overgrow'
+  name: 'Bulbasaur',
+  height: 2.04,
+  types: ['grass','poison'],
+  abilities: 'overgrow'
   },
   {
-    name: 'Weedle',
-    height: 1.00,
-    types: ['bug','poison'],
-    abilities: 'Shield Dust'
+  name: 'Weedle',
+  height: 1.00,
+  types: ['bug','poison'],
+  abilities: 'shield dust'
   },
   {
-    name: 'Jigglypuff',
-    height: 1.08,
-    types: ['normal', 'fairy'],
-    abilities: ['cute charm', 'competitive']
+  name: 'Jigglypuff',
+  height: 1.08,
+  types: ['normal', 'fairy'],
+  abilities: ['cute charm', 'competitive']
   },
   {
-    name: 'Nidoqueen',
-    height: 4.03,
-    types: ['poison', 'ground'],
-    abilities: ['poison point', 'rivalry']
+  name: 'Nidoqueen',
+  height: 4.03,
+  types: ['poison', 'ground'],
+  abilities: ['poison point', 'rivalry']
   }
 ];
 
-for (i = 0; i < pokemonList.length; i++) {
-  document.write('<p>' + pokemonList[i]['name'] + ' (height: ' + pokemonList[i]['height'] + ') <p>');
+function getAll() {
+  return pokemonList;
 }
+
+function add(pokemon) {
+  pokemonList.push(pokemon);
+}
+
+return {
+  getAll: getAll,
+  add: add
+};
+})()
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+document.write('name: ' + pokemon.name + '<br>' + 'height: ' + pokemon.height + '<br>' + 'types: ' + pokemon.types + '<br>' + 'abilities: ' + pokemon.abilities + '<p>');
+})
