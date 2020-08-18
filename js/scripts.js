b8994c376d6f1566403b1bc15fd18089a6108a6d
magicmarie <natukunda162@gmail.com>
2020-08-18 18:03:16 +0300

    create and add a modal

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 var pokemonRepository = (function() {
   var pokemonList = [];
   var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  var modalContainer = document.querySelector('#modal-container');
 
   function getAll() {
     return pokemonList;
@@ -26,7 +27,7 @@ var pokemonRepository = (function() {
 
   function showDetails(pokemon) {
     pokemonRepository.loadDetails(pokemon).then(function(){
-      console.log(pokemon);
+      showModal(pokemon);
     });
   }
 
@@ -53,11 +54,63 @@ var pokemonRepository = (function() {
     }).then(function(details){
       item.imageUrl = details.sprites.front_default;
       item.height = details.height;
-      item.types = Object.keys(details.types);
+      item.types = details.types.map(function(pokemon) {
+        return pokemon.type.name;
+      });
     }).catch(function(e){
       console.error(e);
     });
   }
+
+  function showModal(text) {
+    modalContainer.innerHTML = '';
+
+    let modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'X';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    let titleElement = document.createElement('h1');
+    titleElement.innerText = text.name;
+
+    let imageElement = document.createElement('img');
+    imageElement.setAttribute('src', text.imageUrl);
+
+    let heightElement = document.createElement('p');
+    heightElement.innerText = 'Height: ' + text.height;
+
+    let typesElement = document.createElement('p');
+    typesElement.innerText = 'Types: ' + text.types['0', '1'];
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(imageElement);
+    modal.appendChild(heightElement);
+    modal.appendChild(typesElement);
+    modalContainer.appendChild(modal);
+
+    modalContainer.classList.add('is-visible');
+  }
+
+  function hideModal() {
+    modalContainer.classList.remove('is-visible');
+  }
+
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
+  modalContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    if (target === modalContainer) {
+      hideModal();
+    }
+  });
 
 return {
   getAll: getAll,
@@ -66,6 +119,8 @@ return {
   showDetails: showDetails,
   loadList: loadList,
   loadDetails: loadDetails,
+  showModal: showModal,
+  hideModal: hideModal,
 };
 })()
 
